734536becc61f32bb8f2a68479835f73a9e1e5c2
BilalMushtaq <33187642+BilalMushtaq@users.noreply.github.com>
2017-12-22 23:31:29 +0500

    Create BookAssignment_5-3.py

---
+++ BookAssignment_5-3.py
@@ -0,0 +1,86 @@
+# BookAssignment 5-3 (Alien Colors#1)
+
+# 1st Version
+
+alien_color = 'green'
+
+if(alien_color == 'green'):
+    print("You just earned 5 points!")
+
+# 2nd Version
+
+alien_color = 'red'
+
+if(alien_color == 'green'):
+    print()
+    
+# BookAssignment 5-4 (Alien Colors#2)
+
+# 1st Version
+
+alien_color = 'green'
+
+if(alien_color == 'green'):
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+# 2nd Version
+
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+   
+# BookAssignment 5-5 (Alien Colors#3)
+
+# 1st Version
+
+alien_color = 'green'
+
+if(alien_color == 'green'):
+    print("You just earned 5 points!")
+elif(alien_color == 'yellow'):
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+
+# 2nd Version
+
+alien_color = 'yellow'
+
+if(alien_color == 'green'):
+    print("You just earned 5 points!")
+elif(alien_color == 'yellow'):
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+
+# 3rd Version
+
+alien_color = 'red'
+
+if(alien_color == 'green'):
+    print("You just earned 5 points!")
+elif(alien_color == 'yellow'):
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+    
+# BookAssignment 5-6 (Stages of Life)
+
+age = 40
+if(age < 2):
+    print("The person is a Baby")
+elif(age < 4):
+    print("The person is a Toddler")
+elif(age < 13):
+    print("The peron is a Kid")
+elif(age < 20):
+    print("The person is a Teenager")
+elif(age < 65):
+    print("The person is an Adult")
+else:
+    print("The person is an Elder")
